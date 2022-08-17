ef46f84bae71805cd8e3e2e961a482703a239f6a
Christophe Demarey <christophe.demarey@inria.fr>
2022-08-17 13:33:05 +0200

    add missing path

--- build-md-image.sh
+++ build-md-image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 curl https://get.pharo.org/64/100+vm | bash
-./pharo Pharo.image eval --save load-md-agenda.st
+./pharo Pharo.image eval --save ./Microdown-Agenda/load-md-agenda.st
