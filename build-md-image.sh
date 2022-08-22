18c02d2bfeebb711b141473a4c573eb83d54d0ea
Christophe Demarey <christophe.demarey@inria.fr>
2022-08-22 10:04:59 +0200

    workaround for VM bug

--- build-md-image.sh
+++ build-md-image.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-curl https://get.pharo.org/64/100+vm | bash
-./pharo Pharo.image eval --save ./Microdown-Agenda/load-md-agenda.st
+curl https://get.pharo.org/64/100 | bash
+wget http://files.pharo.org/vm/pharo-spur64/Linux-x86_64/PharoVM-9.0.15-b487900-Linux-x86_64-stockReplacement-bin.zip
+unzip *.zip
+./pharo --headless Pharo.image eval --save ./Microdown-Agenda/load-md-agenda.st
