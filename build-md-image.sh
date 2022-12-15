2738a66867baa1f00c80e64f2167e4867f2bc225
demarey <christophe.demarey@inria.fr>
2022-12-15 16:06:12 +0100

    get VM at that same time as image

--- build-md-image.sh
+++ build-md-image.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-curl https://get.pharo.org/64/100 | bash
-wget http://files.pharo.org/vm/pharo-spur64/Linux-x86_64/PharoVM-9.0.15-b487900-Linux-x86_64-stockReplacement-bin.zip
-unzip *.zip
+curl https://get.pharo.org/64/100+vm | bash
 ./pharo --headless Pharo.image eval --save ./Microdown-Agenda/load-md-agenda.st
