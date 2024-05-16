f1844d4084dcc33bd6c3aff893f894ad09472b9e
DEMAREY Christophe <christophe.demarey@inria.fr>
2024-05-16 11:57:05 +0200

    avoid side effects with double save of the image when evaluating

--- build-md-image.sh
+++ build-md-image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 curl https://get.pharo.org/64/100+vm | bash
-./pharo --headless Pharo.image eval --save ./Microdown-Agenda/load-md-agenda.st
+./pharo Pharo.image eval --save ./Microdown-Agenda/load-md-agenda.st
